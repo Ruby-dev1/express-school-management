93338a99a5fb7719b3d7283df361f639c6e7ac4d
Rubina <rubinasubedi18@gmail.com>
2026-06-28 19:34:20 +0545

    Add school management CRUD APIs with routes

---
+++ src/school management/practice.js
@@ -0,0 +1,46 @@
+ import http from "http";
+ import express from "express";
+ import studentRoutes from "./routes/student.routes.js"
+ import teacherRoutes from "./routes/teacher.routes.js"
+ import subjectRoutes from "./routes/subject.routes.js"
+
+ const app = express();
+
+ const server = http.createServer(app);
+  app.use(express.json())
+
+
+
+
+//  app.get("/",(req,res)=>{
+//     res.send("<h1> SCHOOL--HOME PAGE")
+//  })
+
+//! using routes
+app.use("/students", studentRoutes)
+app.use("/teachers", teacherRoutes)
+app.use("/subjects", subjectRoutes)
+
+
+
+    // res.json({
+    //     message: `students by id ${id} deleted`,
+    //     success: "true",
+        
+    //     data:[{
+    //         id:id,
+    //         name:"Kunal",
+    //     email: "k@gmail.com",
+
+    //     }]
+    // })
+
+
+ 
+
+
+ server.listen(8080, "localhost", ()=>{ 
+   
+    console.log(`server is running at http://localhost:8080`)
+    console.log("press ctrl+c to close the server");
+ })
